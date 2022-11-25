dac064ea4edcab6b45959e01ebdb2cd1d494e754
Vin <fraschkiwitz@protonmail.com>
2022-11-25 14:18:30 +0100

    jscript.js

--- jscript.js
+++ jscript.js
@@ -3,6 +3,7 @@ function capitalizeFirstLetter(string)
 {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
+
 function getComputerChoice () {
     let rndInt = Math.floor(Math.random() * 3);
 
@@ -33,4 +34,54 @@ function getPlayerChoice () {
     
 }
 
-console.log(getPlayerChoice())
+function playRound () {
+    for (let i = 0; i < 5; i++) {
+        singleRound ()
+     }
+}
+
+function singleRound () {
+    let computerSelection = getComputerChoice();
+    let playerSelection = getPlayerChoice();
+
+    console.log(computerSelection);
+    console.log(playerSelection);
+
+   if (playerSelection === computerSelection) {
+        return "Unentschieden";
+   }
+
+   else if  (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "Punkt fuer den Spieler!"
+    }
+
+    else if  (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "Punkt fuer den Computer!"
+    }
+
+    else if  (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "Punkt fuer den Computer!"
+    }
+
+   else if  (playerSelection === "Scissors" && computerSelection === "Paper") {
+         return "Punkt fuer den Spieler!"
+    }
+
+    else if  (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "Punkt fuer den Computer!"
+   }
+
+   else if  (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "Punkt fuer den Spieler!"
+}
+
+}
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+    console.log(singleRound());
+ }
+}
+
+
+console.log(game())
